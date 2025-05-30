075f29e61dddeece7ad441cb12e2aae78fd0e625
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-29 21:00:35 -0600

    Se creo el ejercicio de Eliminar la ultima palabra

---
+++ EJERCICIOPAG61/EliminarlaUltimaPalabra.java
@@ -0,0 +1,9 @@
+
+public class EliminarlaUltimaPalabra {
+    /*4.Desarrollar un programa que elimine 
+    la última palabra de una frase. */
+
+    public static void main(String[] args) {
+    }
+
+}
