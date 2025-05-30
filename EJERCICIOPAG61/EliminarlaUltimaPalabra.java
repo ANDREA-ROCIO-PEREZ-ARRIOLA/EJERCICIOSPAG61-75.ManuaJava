38437f7599cb7b70e537ca6ec2a29ccbfbc7ea50
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-05-29 21:37:09 -0600

    Se realizo la funcionalidad del ejercicio de eliminar la ultima palabra

--- EJERCICIOPAG61/EliminarlaUltimaPalabra.java
+++ EJERCICIOPAG61/EliminarlaUltimaPalabra.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
 public class EliminarlaUltimaPalabra {
     /*4.Desarrollar un programa que elimine 
     la última palabra de una frase. */
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una frase:");
+        String frase = scanner.nextLine().trim();
+
+        int ultimaPosicion = frase.lastIndexOf(" ");
+
+        String resultado;
+        if (ultimaPosicion != -1) {
+            resultado = frase.substring(0, ultimaPosicion);
+        } else {
+            resultado = frase;
+        }
+
+        System.out.println("Frase sin la última palabra queda así: " + resultado);
+
+        scanner.close();
+
     }
 
 }
